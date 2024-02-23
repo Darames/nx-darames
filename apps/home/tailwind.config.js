a5549a095a1dfd1bd42e800d504b9efba01b0855
MarcHiltenPX <marc.hilten@pixelgenau.com>
2024-02-23 14:36:40 +0100

    proxicard changes

--- apps/home/tailwind.config.js
+++ apps/home/tailwind.config.js
@@ -9,17 +9,21 @@ module.exports = {
   ],
   theme: {
     colors: {
+      transparent: 'transparent',
       red: 'red',
       black: 'black',
-      transparent: 'transparent'
+      proxcard: {
+        border:'hsl(280 10% 50% / 1)',
+        card: 'hsl(237 36% 10%)',
+        text: 'hsl(240 18% 80%)',
+        container: 'hsl(246 44% 7%)'
+      },
     },
     extend: {
       backgroundImage: {
-        proxcard: {
-          border: 'hsl(280 10% 50% / 1)',
-          bg: 'conic-gradient(from 180deg at 50% 70%,hsla(0,0%,98%,1) 0deg,#eec32d 72.0000010728836deg,#ec4b4b 144.0000021457672deg,#709ab9 216.00000858306885deg,#4dffbf 288.0000042915344deg,hsla(0,0%,98%,1) 1turn);'
-        }
-      }
+        'proxcard-gradient-2': 'conic-gradient(from 180deg at 50% 70%,hsla(0,0%,98%,1) 0deg,#eec32d 72.0000010728836deg,#ec4b4b 144.0000021457672deg,#709ab9 216.00000858306885deg,#4dffbf 288.0000042915344deg,hsla(0,0%,98%,1) 1turn);',
+        'proxcard-gradient': 'linear-gradient(135deg, #d0e4f7 0%,#73b1e7 24%,#0a77d5 50%,#539fe1 79%,#87bcea 100%);'
+      },
     },
   },
   plugins: [],
