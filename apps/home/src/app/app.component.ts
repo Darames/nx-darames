53ed0971eb780c6b2b31f120b853ffa684ee43d3
MarcHiltenPX <marc.hilten@pixelgenau.com>
2024-02-23 10:04:10 +0100

    move files

--- apps/home/src/app/app.component.ts
+++ apps/home/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { ProximityGlowCardsComponent } from './proximity-glow-cards.component';
+import { ProximityGlowCardsComponent } from 'libs/ui/src/lib/proximity-glow-cards/proximity-glow-cards.component';
 
 @Component({
   standalone: true,
   imports: [NxWelcomeComponent, RouterModule, ProximityGlowCardsComponent],
-  selector: 'angular-monorepo-root',
+  selector: 'home-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ apps/home/tailwind.config.js
@@ -0,0 +1,26 @@
+const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
+const { join } = require('path');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    colors: {
+      red: 'red',
+      black: 'black',
+      transparent: 'transparent'
+    },
+    extend: {
+      backgroundImage: {
+        proxcard: {
+          border: 'hsl(280 10% 50% / 1)',
+          bg: 'conic-gradient(from 180deg at 50% 70%,hsla(0,0%,98%,1) 0deg,#eec32d 72.0000010728836deg,#ec4b4b 144.0000021457672deg,#709ab9 216.00000858306885deg,#4dffbf 288.0000042915344deg,hsla(0,0%,98%,1) 1turn);'
+        }
+      }
+    },
+  },
+  plugins: [],
+};
